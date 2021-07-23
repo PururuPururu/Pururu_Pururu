b61d57a34e2ce87ecd1d712ce67c0c61384cbdb5
PururuPururu <76193299+PururuPururu@users.noreply.github.com>
2021-07-23 15:13:20 +0500

    Сделато

---
+++ Lesson4_task2.py
@@ -0,0 +1,19 @@
+from requests import get, utils
+import datetime
+
+response = utils.get_unicode_from_response(get('https://www.nationalbank.kz/rss/rates_all.xml'))  #Получаем данные курсов валют нац банка Казахстана
+
+
+def currency_rates(code):
+    response_content = response.split("item")                                                     #Сплитим на блоки, относящиеся к каждой конкретной валюте
+    for i in response_content:                                                                    #Исправляем регистр строки-аргумента нашей функциии currency_rates()
+        if code.upper() in i:
+            currency = float(i.replace("/", "").split("<description>")[-2])                       #Извлекаем курс валюты
+            actual_time = (i.replace("/", "").split("<pubDate>")[-2])                             #Извлекаем дату
+            format = "%d.%m.%Y"                                                                   #Задаем формат для метода strptime(), в этом формате содержится дата в строке.
+            date_time = datetime.datetime.strptime(actual_time, format).date()                    #Преобразуем строку actual_time из строки в объект datetime и затем, в объект date
+    return currency, date_time                                                                    #Выдаем результат
+
+
+currency1, date_time1 = (currency_rates("EUR"))                                                   #Получаем результат работы функции currency_rates и выводим его на экран
+print(currency1, date_time1)
