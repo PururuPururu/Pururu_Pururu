a830af1604471e0a6c8673c71044a7218b08c8e7
PururuPururu <76193299+PururuPururu@users.noreply.github.com>
2021-08-02 22:48:22 +0500

    Delete task_2_4.py

--- task_2_4.py
+++
@@ -1,9 +0,0 @@
-workers = ['инженер-конструктор Игорь',
-           'главный бухгалтер МАРИНА',
-           'токарь высшего разряда нИКОЛАй',
-           'директор аэлита']
-
-counter1 = 0
-while counter1 < len(workers):
-    print('Привет, ', workers[counter1].split(' ')[-1].capitalize())
-    counter1 += 1
